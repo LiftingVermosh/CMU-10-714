4fb1958b1345bbfb08a030a486e84548b1cf48dd
LiftingVermosh <fallingstar233@163.com>
2025-09-03 22:40:56 +0800

    Initial Commit

---
+++ Assignments/hw0/src/simple_ml.py
@@ -0,0 +1,192 @@
+import struct
+import numpy as np
+import gzip
+try:
+    from simple_ml_ext import *
+except:
+    pass
+
+
+def add(x, y):
+    """ A trivial 'add' function you should implement to get used to the
+    autograder and submission system.  The solution to this problem is in the
+    the homework notebook.
+
+    Args:
+        x (Python number or numpy array)
+        y (Python number or numpy array)
+
+    Return:
+        Sum of x + y
+    """
+    return x + y
+
+
+def parse_mnist(image_filename, label_filename):
+    """ 读取 MNIST 格式的图像和标签文件。 请参见此页：
+    http://yann.lecun.com/exdb/mnist/ 获取文件格式说明。
+
+    参数：
+        image_filename (str)：以 MNIST 格式压缩的图像文件的名称
+        label_filename (str)：以 MNIST 格式压缩的标签文件名称
+
+    返回值
+        元组 (X,y)：
+            X（numpy.ndarray[np.float32]）：包含加载数据的 2D numpy 数组。 
+                数据的二维 numpy 数组。 数据的维度应为 
+                (num_examples x input_dim)，其中 "input_dim "是数据的全维度，例如 
+                例如，由于 MNIST 图像的尺寸是 28x28，所以它 
+                为 784。 值的类型应为 np.float32，数据 
+                的最小值为 0.0，最大值为 1.0。 
+                最大值为 1.0（即，将原始值 0 缩放为 0.0，将 255 缩放为 1.0 
+                和 255 分辨率为 1.0）。
+
+            y（numpy.ndarray[dtype=np.uint8]）： 1D numpy 数组，包含
+                数组。 值的类型应为 np.uint8，且
+                对于 MNIST 将包含 0-9 的值。
+        """
+    import struct
+    import numpy as np
+    import gzip
+
+    # Load the images and labels
+    with gzip.open(label_filename, 'rb') as f:
+        magic, num_images = struct.unpack('>II', f.read(8))     # 当前架构为 x86 64 大端序 且前8个字节为魔数和图像数量
+        if magic != 2049:
+            raise ValueError('非法的标签魔数: {}'.format(magic))
+        labels = np.frombuffer(f.read(), dtype=np.uint8)
+
+    with gzip.open(image_filename, 'rb') as f:
+        magic, num_images, num_rows, num_cols = struct.unpack('>IIII', f.read(16))
+        if magic != 2051:
+            raise ValueError('非法的图像魔数: {}'.format(magic))
+        images = np.frombuffer(f.read(), dtype=np.uint8).reshape(num_images, num_rows*num_cols)
+
+    # Normalize the images to [0,1]
+    images = images.astype(np.float32) / 255.0
+    return images, labels
+
+
+def softmax_loss(Z, y):
+    """ Return softmax loss.  Note that for the purposes of this assignment,
+    you don't need to worry about "nicely" scaling the numerical properties
+    of the log-sum-exp computation, but can just compute this directly.
+
+    Args:
+        Z (np.ndarray[np.float32]): 2D numpy array of shape
+            (batch_size, num_classes), containing the logit predictions for
+            each class.
+        y (np.ndarray[np.uint8]): 1D numpy array of shape (batch_size, )
+            containing the true label of each example.
+
+    Returns:
+        Average softmax loss over the sample.
+    """
+    ### BEGIN YOUR CODE
+    pass
+    ### END YOUR CODE
+
+
+def softmax_regression_epoch(X, y, theta, lr = 0.1, batch=100):
+    """ Run a single epoch of SGD for softmax regression on the data, using
+    the step size lr and specified batch size.  This function should modify the
+    theta matrix in place, and you should iterate through batches in X _without_
+    randomizing the order.
+
+    Args:
+        X (np.ndarray[np.float32]): 2D input array of size
+            (num_examples x input_dim).
+        y (np.ndarray[np.uint8]): 1D class label array of size (num_examples,)
+        theta (np.ndarrray[np.float32]): 2D array of softmax regression
+            parameters, of shape (input_dim, num_classes)
+        lr (float): step size (learning rate) for SGD
+        batch (int): size of SGD minibatch
+
+    Returns:
+        None
+    """
+    ### BEGIN YOUR CODE
+    pass
+    ### END YOUR CODE
+
+
+def nn_epoch(X, y, W1, W2, lr = 0.1, batch=100):
+    """ Run a single epoch of SGD for a two-layer neural network defined by the
+    weights W1 and W2 (with no bias terms):
+        logits = ReLU(X * W1) * W2
+    The function should use the step size lr, and the specified batch size (and
+    again, without randomizing the order of X).  It should modify the
+    W1 and W2 matrices in place.
+
+    Args:
+        X (np.ndarray[np.float32]): 2D input array of size
+            (num_examples x input_dim).
+        y (np.ndarray[np.uint8]): 1D class label array of size (num_examples,)
+        W1 (np.ndarray[np.float32]): 2D array of first layer weights, of shape
+            (input_dim, hidden_dim)
+        W2 (np.ndarray[np.float32]): 2D array of second layer weights, of shape
+            (hidden_dim, num_classes)
+        lr (float): step size (learning rate) for SGD
+        batch (int): size of SGD minibatch
+
+    Returns:
+        None
+    """
+    ### BEGIN YOUR CODE
+    pass
+    ### END YOUR CODE
+
+
+
+### CODE BELOW IS FOR ILLUSTRATION, YOU DO NOT NEED TO EDIT
+
+def loss_err(h,y):
+    """ Helper funciton to compute both loss and error"""
+    return softmax_loss(h,y), np.mean(h.argmax(axis=1) != y)
+
+
+def train_softmax(X_tr, y_tr, X_te, y_te, epochs=10, lr=0.5, batch=100,
+                  cpp=False):
+    """ Example function to fully train a softmax regression classifier """
+    theta = np.zeros((X_tr.shape[1], y_tr.max()+1), dtype=np.float32)
+    print("| Epoch | Train Loss | Train Err | Test Loss | Test Err |")
+    for epoch in range(epochs):
+        if not cpp:
+            softmax_regression_epoch(X_tr, y_tr, theta, lr=lr, batch=batch)
+        else:
+            softmax_regression_epoch_cpp(X_tr, y_tr, theta, lr=lr, batch=batch)
+        train_loss, train_err = loss_err(X_tr @ theta, y_tr)
+        test_loss, test_err = loss_err(X_te @ theta, y_te)
+        print("|  {:>4} |    {:.5f} |   {:.5f} |   {:.5f} |  {:.5f} |"\
+              .format(epoch, train_loss, train_err, test_loss, test_err))
+
+
+def train_nn(X_tr, y_tr, X_te, y_te, hidden_dim = 500,
+             epochs=10, lr=0.5, batch=100):
+    """ Example function to train two layer neural network """
+    n, k = X_tr.shape[1], y_tr.max() + 1
+    np.random.seed(0)
+    W1 = np.random.randn(n, hidden_dim).astype(np.float32) / np.sqrt(hidden_dim)
+    W2 = np.random.randn(hidden_dim, k).astype(np.float32) / np.sqrt(k)
+
+    print("| Epoch | Train Loss | Train Err | Test Loss | Test Err |")
+    for epoch in range(epochs):
+        nn_epoch(X_tr, y_tr, W1, W2, lr=lr, batch=batch)
+        train_loss, train_err = loss_err(np.maximum(X_tr@W1,0)@W2, y_tr)
+        test_loss, test_err = loss_err(np.maximum(X_te@W1,0)@W2, y_te)
+        print("|  {:>4} |    {:.5f} |   {:.5f} |   {:.5f} |  {:.5f} |"\
+              .format(epoch, train_loss, train_err, test_loss, test_err))
+
+
+
+if __name__ == "__main__":
+    X_tr, y_tr = parse_mnist("data/train-images-idx3-ubyte.gz",
+                             "data/train-labels-idx1-ubyte.gz")
+    X_te, y_te = parse_mnist("data/t10k-images-idx3-ubyte.gz",
+                             "data/t10k-labels-idx1-ubyte.gz")
+
+    print("Training softmax regression")
+    train_softmax(X_tr, y_tr, X_te, y_te, epochs=10, lr = 0.1)
+
+    print("\nTraining two layer neural network w/ 100 hidden units")
+    train_nn(X_tr, y_tr, X_te, y_te, hidden_dim=100, epochs=20, lr = 0.2)
